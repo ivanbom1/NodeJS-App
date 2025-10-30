7fcbb599ef4f2fe849c42a755c15464542a532e9
ivan.bomko <ivan.bomko@epita.fr>
2025-10-30 21:11:09 +0100

    gitignore and js files commit

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from "express"
+
+const app = express()
+
+const PORT = 3000
+
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.json({
+	    firstName: "John",
+	    lastName: "Doe"
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
+})
