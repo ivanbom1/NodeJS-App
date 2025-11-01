84b6bba0304b32cdfde7c1d481fcb7cf3a1ae1f4
ivan.bomko <ivan.bomko@epita.fr>
2025-11-01 22:11:15 +0100

    data flow and middleware tested

--- server.js
+++ server.js
@@ -1,16 +1,37 @@
 import express from "express"
-
 const app = express()
-
 const PORT = 3000
+
+const users = [
+    { id: 1, name: "Alice" },
+    { id: 2, name: "Bob" },
+    { id: 3, name: "Charlie" },
+    { id: 4, name: "Dave" },
+]
 
 app.use(express.json())
 
+// middleware to log request body
+app.use(async (req, res, next) => {
+	const date = new Date().toISOString()
+	console.log(`[${date}] ${req.method} ${req.url}`)
+
+	
+	const response = await fetch("https://jsonplaceholder.typicode.com/users/1")
+	const data = await response.json()
+	req.data = data
+	console.log(data)
+	
+	next()
+})
+
 app.get("/", (req, res) => {
-    res.json({
-	    firstName: "John",
-	    lastName: "Doe"
-    })
+	const data = req.data
+	res.json({ users, data })
+})
+
+app.post("/", (req, res) => {
+    console.log(req)
 })
 
 app.listen(PORT, () => {
